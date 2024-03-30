ab76e4f1dadc5ae02471ab5a22ba965d8632eb7c
asif <aasifkhan02@gmail.com>
2024-03-30 14:42:13 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative LLama2-CPU Project',
+    version = '0.0.0',
+    autho = "Asif-AI",
+    author_email = 'aasifkhan02@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
