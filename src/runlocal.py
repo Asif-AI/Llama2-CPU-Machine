b46b761206b51abea01d1efc737acbc5295afb07
asif <aasifkhan02@gmail.com>
2024-03-31 00:44:25 +0530

    app.py added

--- src/runlocal.py
+++ src/runlocal.py
@@ -6,7 +6,8 @@ from src.helper import *
 B_INST, E_INST = "[INST]" , "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<<SYS>>\n\n"
 
-instruction =  "Convert the following text from English to Hindi: \n\n {text}"
+##instruction =  "Convert the following text from English to Hindi: \n\n {text}"
+instruction =  "Give a proper summary of the book: \n\n {text}"
 
 SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
@@ -21,4 +22,4 @@ llm = CTransformers(model = "model/llama-2-7b-chat.ggmlv3.q4_0.bin",
 
 LLM_Chain = LLMChain(prompt=prompt, llm=llm)
 
-print(LLM_Chain.run("How are you?"))
+print(LLM_Chain.run("Harry Potter"))
